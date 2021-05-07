cfc92f063cfbfaa107f7ac28c609163e6aa61075
xzhangkellogg <81667306+xzhangkellogg@users.noreply.github.com>
2021-05-07 13:02:07 -0500

    Initial commit

---
+++ netlify/functions/movies.js
@@ -0,0 +1,47 @@
+// allows us to read csv files
+let csv = require('neat-csv')
+
+// allows us to read files from disk
+let fs = require('fs')
+
+// defines a lambda function
+exports.handler = async function(event) {
+  // write the event object to the back-end console
+  console.log(event)
+
+  // read movies CSV file from disk
+  let moviesFile = fs.readFileSync(`./movies.csv`)
+  
+  // turn the movies file into a JavaScript object, wait for that to happen
+  let moviesFromCsv = await csv(moviesFile)
+
+  // write the movies to the back-end console, check it out
+  console.log(moviesFromCsv)
+
+  // 🔥 hw6: your recipe and code starts here!
+  let year = event.queryStringParameters.year
+  let genre = event.queryStringParameters.genre
+  
+  if (year == undefined || genre == undefined) {
+    return {
+      statusCode: 200, // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
+      body: `Nope!` // a string of data
+    }
+  }
+  else {
+    let returnValue = {
+      numResults: 0,
+      movies: []
+    }
+
+    for (let i=0; i < moviesFromCsv.length; i++) {
+
+    }
+
+    // a lambda function returns a status code and a string of data
+    return {
+      statusCode: 200, // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
+      body: `Hello from the back-end!` // a string of data
+    }
+  }
+}
